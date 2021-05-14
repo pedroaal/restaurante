a535914f43a74adc1052778657a9d55771a6de1e
PedroAltamirano <pedroaal04@gmail.com>
2021-05-14 14:34:00 -0500

    user model

---
+++ src/models/user.js
@@ -0,0 +1,87 @@
+import mongoose, {Schema} from 'mongoose';
+
+const MODEL_NAME = 'User';
+
+const schema = new Schema(
+  // columnas de la tabla
+  {
+    // id: {
+    //   type: ObjectId,
+    //   required: true,
+    // },
+    ci: {
+      type: String,
+      required: true,
+      match: /[/d]/,
+      index: {
+        unique: true,
+        sparse: true
+      }
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      index: {
+        unique: true,
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      // required: true,
+      max: 7
+    },
+    movil: {
+      type: String,
+      // required: true,
+      max: 10
+    },
+    address: {
+      type: String,
+      // required: true,
+    },
+    genre: {
+      type: Boolean,
+      // required: true,
+    },
+    birthday: {
+      type: Date,
+      // required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: 0,
+    },
+    role_id: {
+      type: Schema.ObjectId,
+      required: true,
+    },
+    store_id: {
+      type: Schema.ObjectId,
+      // required: true,
+    },
+    status: {
+      type: Boolean,
+      required: true,
+      default: 1,
+    },
+  },
+  // created_at, updated_at
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.models[MODEL_NAME] || mongoose.model(MODEL_NAME, schema, 'users');
