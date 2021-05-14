a69bbd3c327fdb549f39ad11657a8c70aa9d10f8
PedroAltamirano <pedroaal04@gmail.com>
2021-05-14 12:12:03 -0500

    added mongoose

--- util/dbConnect.js
+++ util/dbConnect.js
@@ -1,16 +1,10 @@
-import { MongoClient } from 'mongodb'
+import mongoose from 'mongoose'
 
-const { MONGODB_URI, MONGODB_DB } = process.env
+const MONGODB_URI = process.env.MONGODB_URI
 
 if (!MONGODB_URI) {
   throw new Error(
     'Please define the MONGODB_URI environment variable inside .env.local'
-  )
-}
-
-if (!MONGODB_DB) {
-  throw new Error(
-    'Please define the MONGODB_DB environment variable inside .env.local'
   )
 }
 
@@ -19,13 +13,13 @@ if (!MONGODB_DB) {
  * in development. This prevents connections growing exponentially
  * during API Route usage.
  */
-let cached = global.mongo
+let cached = global.mongoose
 
 if (!cached) {
-  cached = global.mongo = { conn: null, promise: null }
+  cached = global.mongoose = { conn: null, promise: null }
 }
 
-export async function connectToDatabase() {
+async function dbConnect() {
   if (cached.conn) {
     return cached.conn
   }
@@ -34,15 +28,18 @@ export async function connectToDatabase() {
     const opts = {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      bufferCommands: false,
+      bufferMaxEntries: 0,
+      useFindAndModify: false,
+      useCreateIndex: true,
     }
 
-    cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
-      return {
-        client,
-        db: client.db(MONGODB_DB),
-      }
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose
     })
   }
   cached.conn = await cached.promise
   return cached.conn
 }
+
+export default dbConnect
