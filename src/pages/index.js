27ebb33253bdb6b1aa9c589ae94a9ef70de301c2
PedroAltamirano <pedroaal04@gmail.com>
2021-05-15 15:03:56 -0500

    session

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,23 @@ import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
 import {useState, useEffect} from 'react';
 import {dbConnect, jsonify } from '@/util/dbConnect';
-import User from '@/models/user';
+// import User from '@/models/user';
+// import Product from '@/models/product';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 export async function getServerSideProps(context) {
-  dbConnect();
-  const users = await User.find({}).exec();
+  await dbConnect();
+  
+  // const products = await Product.find({}).exec();
 
   return {
     props: {
-      users: jsonify(users),
+      // products: jsonify(products),
     }
   }
 }
 
-export default function Home({users}) {
+export default function Home({}) {
   // const [asyncUsers, setAsyncUsers] = useState([]);
   // const [loading, setLoading] = useState(true);
 
@@ -31,6 +34,8 @@ export default function Home({users}) {
   //   }, 1000);
   // }, [])
 
+  const [ session, loading ] = useSession();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -39,67 +44,18 @@ export default function Home({users}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {isConnected ? (
-        <h2 className="subtitle">You are connected to MongoDB</h2>
-      ) : (
-        <h2 className="subtitle">
-          You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
-          for instructions.
-        </h2>
-      )}
-
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {!session && <>
+          Not signed in <br/>
+          <button onClick={() => signIn()}>Sign in</button>
+        </>}
+        {session && <>
+          Signed in as {session.user.email} <br/>
+          <button onClick={() => signOut()}>Sign out</button>
+        </>}        
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
       </footer>
     </div>
   )
