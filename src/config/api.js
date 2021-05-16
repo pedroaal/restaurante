9be7bc1f766421c9bc1f8044c683eb9406ce9a70
PedroAltamirano <pedroaal04@gmail.com>
2021-05-15 23:30:45 -0500

    base url

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${process.env.NEXTAUTH_URL}/api/`,
+  baseURL: 'http://localhost:3000/api/',
 });
 
 // api.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
