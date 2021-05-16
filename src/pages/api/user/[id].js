64e958250f6f7f3dbf1100677c1c7d1cd2733711
PedroAltamirano <pedroaal04@gmail.com>
2021-05-15 20:50:50 -0500

    initial config

--- src/pages/api/user/[id].js
+++ src/pages/api/user/[id].js
@@ -1,20 +1,25 @@
-// export default function userHandler(req, res) {
-//   const {
-//     query: { id, name },
-//     method,
-//   } = req
+import {dbConnect} from '@/util/dbConnect';
+import User from '@/models/user';
 
-//   switch (method) {
-//     case 'GET':
-//       // Get data from your database
-//       res.status(200).json({ id, name: `User ${id}` })
-//       break
-//     case 'PUT':
-//       // Update or create data in your database
-//       res.status(200).json({ id, name: name || `User ${id}` })
-//       break
-//     default:
-//       res.setHeader('Allow', ['GET', 'PUT'])
-//       res.status(405).end(`Method ${method} Not Allowed`)
-//   }
-// }
+export default async function(req, res) {
+  await dbConnect()
+  const {
+    query: { id },
+    method,
+  } = req
+  
+  switch (method) {
+    case 'GET':
+      const user = await User.find({email: id});
+      console.log(user);
+      res.status(200).json()
+      break
+    case 'PUT':
+      // Update or create data in your database
+      res.status(200).json()
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'PUT'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
