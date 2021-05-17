51b145072088776d0803d05da2a9e3b203f38d68
PedroAltamirano <pedroaal04@gmail.com>
2021-05-16 20:03:16 -0500

    fix

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -15,22 +15,25 @@ const options = {
       },
       async authorize(credentials) {
         await dbConnect();
-        console.log('cred', credentials);
-        const user = await User.findOne({email: credentials.email}, (err, result) => {
+        // console.log('cred', credentials);
+        const user = await User.findOne({email: credentials.email, status: true}, (err, result) => {
           if (err){ 
             console.log(err);
             return null;
           }
         });
+
         if(user){
-          console.log(user.password);
           if(await argon2.verify(user.password, credentials.password)) {
+            // console.log(user);
             return user;
           } else {
+            console.log('error');
             return null;
           }
+        } else {
+          return null;
         }
-        return null;
       }
     })
   ],
@@ -38,21 +41,18 @@ const options = {
     jwt: true,
     maxAge: 2 * 60 * 60, // 2 horas
   },
-  // jwt: {
-  //   // signingKey: {"kty":"oct","kid":"Dl893BEV-iVE-x9EC52TDmlJUgGm9oZ99_ZL025Hc5Q","alg":"HS512","k":"K7QqRmJOKRK2qcCKV_pi9PSBv3XP0fpTu30TP8xn4w01xR3ZMZM38yL2DnTVPVw6e4yhdh0jtoah-i4c_pZagA"},
-  //   encryption: true,
-  //   // encryptionKey: "",
-  //   // decryptionKey = encryptionKey,
-  //   // decryptionOptions = {
-  //   //    algorithms: ['A256GCM']
-  //   // },
-  // },
+  jwt: {
+    signingKey: process.env.JWT_KEY,
+    encryption: true,
+    // encryptionKey: "pedro",
+    // decryptionKey: encryptionKey,
+  },
   pages: {
     signIn: '/auth/signin',
     // signOut: '/auth/signout',
     // error: '/auth/error',
     verifyRequest: '/auth/verify-request',
-    // newUser: null
+    // newUser: '/auth/signup',
   }
 }
 
