096fbf69ae6d525c632b0653e98d560fb1ff1a55
PedroAltamirano <pedroaal04@gmail.com>
2021-11-21 16:44:49 -0500

    add refresh

--- src/components/organisms/categories.js
+++ src/components/organisms/categories.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { baseAPI } from '@/config/api';
 import { connect } from 'react-redux';
-import { setFiltered } from '@/redux/actions'
+import { setFiltered } from '@/redux/actions';
+
+import { FaUndo } from 'react-icons/fa';
 
 export async function getServerSideProps(context) {
   return {
@@ -10,33 +12,39 @@ export async function getServerSideProps(context) {
   }
 }
 
-function Categories({products, filtered, setFiltered}) {
+function Categories({ products, filtered, setFiltered }) {
   const [loading, setLoading] = useState(true);
   const [categories, setCategories] = useState([]);
-  
+
   useEffect(async () => {
     fetch(`${baseAPI}products/getCategories`)
-    .then(res => res.json())
-    .then(categories => {
-      setCategories(categories);
-      setLoading(false);
-    });
+      .then(res => res.json())
+      .then(categories => {
+        setCategories(categories);
+        setLoading(false);
+      });
   }, [])
 
   const skeleton = () => (
-    [1,2,3].map(i => (
+    [1, 2, 3].map(i => (
       <button className="btn-sm bg-gray-300 animate-pulse h-8 w-1/3" key={i}></button>
     ))
   );
 
   const filterProducts = key => {
-    console.log(key);
-    console.log(products.filter(prod => prod.category_id == key))
-    setFiltered(products.filter(prod => prod.category_id == key))
+    if (key) {
+      setFiltered(products.filter(prod => prod.category_id == key))
+      return
+    }
+
+    setFiltered([...products])
   }
 
   return (
     <div className='flex flex-nowrap w-full p-1 overflow-x-auto'>
+      <button className="btn-sm bg-gray-200" key='refresh' onClick={() => filterProducts(null)}>
+        <FaUndo />
+      </button>
       {loading ? skeleton() : categories.map((cat) => (
         <button className="btn-sm bg-gray-200" key={cat._id} onClick={() => filterProducts(cat._id)}>{cat.name}</button>
       ))}
