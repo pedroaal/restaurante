ce1598e091a4097f78ff6bfe2ee2fbb6c011f055
PedroAltamirano <pedroaal04@gmail.com>
2021-11-21 16:45:47 -0500

    link to cart

--- src/components/organisms/footer.js
+++ src/components/organisms/footer.js
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { BiCartAlt, BiBarcodeReader } from "react-icons/bi";
 import Link from 'next/link';
+import { FaShoppingCart } from 'react-icons/fa';
 
 export async function getServerSideProps(context) {
   return {
@@ -19,9 +20,9 @@ export default function Footer() {
           </svg>
         </div>
         <div className='p-4 w-1/2 flex justify-center'>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-          </svg>
+          <Link href='/cart'>
+            <FaShoppingCart />
+          </Link>
         </div>
       </div>
     </>
