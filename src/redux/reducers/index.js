168b10bc78da5ef0e78b8e862f5d5db686b26113
PedroAltamirano <pedroaal04@gmail.com>
2021-11-21 17:45:17 -0500

    import paths

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import cartReducer from '@/reducers/cartReducer';
-import productReducer from '@/reducers/productReducer';
+import cartReducer from '@reducers/cartReducer';
+import productReducer from '@reducers/productReducer';
 
 const rootReducer = combineReducers({
   cartReducer, productReducer
