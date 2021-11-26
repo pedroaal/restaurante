1a916cfdd4a9065d4e48393da6af1c3f4b7e4dee
PedroAltamirano <pedroaal04@gmail.com>
2021-11-26 12:22:02 -0500

    to build

--- src/pages/api/template.js
+++ src/pages/api/template.js
@@ -1,37 +1,37 @@
-import { dbConnect } from '@/utils/dbConnect';
-import Model from '@/models/model';
+// import { dbConnect } from '@/utils/dbConnect';
+// import Model from '@/models/model';
 
-export default async function (req, res) {
-  await dbConnect()
-  const { method } = req
-  const { id } = req.query
+// export default async function (req, res) {
+//   await dbConnect()
+//   const { method } = req
+//   const { id } = req.query
 
-  switch (method) {
-    case 'GET':
-      const response = await Model.findById(id);
-      res.status(200).json(response)
-      break
-    case 'POST':
-      const newModel = new Model(req.body);
-      newModel.save(err => {
-        if (err) return res.status(500).send(err);
-        return res.status(200).send(newModel);
-      });
-      break
-    case 'PUT':
-      // response = 'ok'
-      // response = await Order.findById(id);
-      // console.log(response);
-      res.status(200).json('response')
-      break
-    case 'DELETE':
-      // response = 'ok'
-      // response = await Order.findById(id);
-      // console.log(response);
-      res.status(200).json('response')
-      break
-    default:
-      res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE'])
-      res.status(405).end(`Method ${method} Not Allowed`)
-  }
-}
+//   switch (method) {
+//     case 'GET':
+//       const response = await Model.findById(id);
+//       res.status(200).json(response)
+//       break
+//     case 'POST':
+//       const newModel = new Model(req.body);
+//       newModel.save(err => {
+//         if (err) return res.status(500).send(err);
+//         return res.status(200).send(newModel);
+//       });
+//       break
+//     case 'PUT':
+//       // response = 'ok'
+//       // response = await Order.findById(id);
+//       // console.log(response);
+//       res.status(200).json('response')
+//       break
+//     case 'DELETE':
+//       // response = 'ok'
+//       // response = await Order.findById(id);
+//       // console.log(response);
+//       res.status(200).json('response')
+//       break
+//     default:
+//       res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE'])
+//       res.status(405).end(`Method ${method} Not Allowed`)
+//   }
+// }
