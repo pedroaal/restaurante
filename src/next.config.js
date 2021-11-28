0ceb1f180ad5ad64761d1480ce4165db892e7c19
PedroAltamirano <pedroaal04@gmail.com>
2021-11-27 20:28:10 -0500

    config

---
+++ src/next.config.js
@@ -0,0 +1,14 @@
+// const withPWA = require('next-pwa')
+// const runtimeCaching = require('next-pwa/cache')
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   pwa: {
+//     dest: 'public',
+//     runtimeCaching,
+//   },
+// })
+
+module.exports = {
+  reactStrictMode: true,
+}
